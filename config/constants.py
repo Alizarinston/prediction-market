25397cecf8432a7260dc5f83b85353f5f380078b
AverHLV <moc9.cs4ever@gmail.com>
2019-09-07 20:48:04 +0300

    OWM client update, tests

--- config/constants.py
+++ config/constants.py
@@ -3,3 +3,6 @@ from unipath import Path
 # file paths
 base_dir = Path(__file__).absolute().ancestor(2)
 secret_filename = base_dir.child('config').child('secret.json')
+
+# utils
+owm_timeout = 60
--- utils.py
+++ utils.py
@@ -1,9 +1,10 @@
 import logging
 
 from django.core.exceptions import ImproperlyConfigured
-from json import loads
-from config import constants
 from requests import get, exceptions
+from json import loads
+
+from config import constants
 
 logger = logging.getLogger('custom')
 
@@ -28,43 +29,45 @@ def get_secret(filename):
         raise ImproperlyConfigured('Add the {0} field to json secret'.format(k))
 
 
-secret_dict = get_secret(constants.secret_filename)
-
-
 class OpenWeatherMap(object):
+    """ Open weather map API client """
+
+    _uri = 'https://api.openweathermap.org/data/2.5/weather?units=metric&appid={}&q='
 
     def __init__(self, appid: str):
-        self.uri = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
+        self._uri = self._uri.format(appid)
 
     def _request(self, uri: str) -> dict:
         """ Make GET request to given uri """
 
         try:
-            return self._parse_errors(get(uri).json())
+            return self._parse_errors(get(uri, timeout=constants.owm_timeout).json())
 
         except exceptions.Timeout:
             logger.critical('Timeout occurred')
 
+        except exceptions.HTTPError as e:
+            logger.critical('Http error: {}'.format(e))
+
         except exceptions.ConnectionError as e:
             logger.critical('Connection error: {}'.format(e))
 
-    def city_temperature(self, city: str) -> dict:
+    def city_temperature(self, city: str) -> float:
         """ Retrieve temperature info """
-        try:
-            return self._request(self.uri.format(city))["main"]["temp"]
 
-        except KeyError:
-            logger.critical('Requests returned empty dictionary')
+        response = self._request(self._uri + city)
+
+        if response is not None:
+            return response['main']['temp']
 
     @staticmethod
-    def _parse_errors(response: dict) -> dict:
-        if 'errors' in response:
-            for error in response['errors']:
-                logger.warning('OpenWeatherMap response error: {}'.format(error))
-
-            return {}
+    def _parse_errors(response: dict) -> (dict, None):
+        if response['cod'] != 200:
+            logger.warning('OpenWeatherMap response error, code: {}'.format(response['cod']))
+            return
 
         return response
 
 
+secret_dict = get_secret(constants.secret_filename)
 owm = OpenWeatherMap(secret_dict['owm_id'])
