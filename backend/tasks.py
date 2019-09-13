eeb26b71428ea0795bab5649f70717d33b872837
alizarinston <alizarinstone@gmail.com>
2019-09-13 16:04:45 +0300

    add tasks.py

---
+++ backend/tasks.py
@@ -0,0 +1,13 @@
+from celery import shared_task
+from backend.models import Market
+import datetime
+
+
+@shared_task(name="predict_weather")
+def predict_weather(name):
+    Market.objects.create(name=name,
+                          b=100,
+                          start_date=datetime.datetime.now(),
+                          end_date=datetime.datetime.now() + datetime.timedelta(days=1),
+                          resolved=False,
+                          outcomes_description='1;2')
--- config/celery.py
+++ config/celery.py
@@ -8,7 +8,7 @@ try:
     environ['DJANGO_SETTINGS_MODULE']
 
 except KeyError:
-    environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
+    environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
 
 settings = import_module(environ['DJANGO_SETTINGS_MODULE'])
 
@@ -18,3 +18,11 @@ app = Celery(settings.BASE_NAME)
 app.config_from_object(settings)
 app.autodiscover_tasks()
 app.conf.task_default_queue = 'default'
+
+app.conf.beat_schedule = {
+    'predict-weather-every-day': {
+        'task': 'predict_weather',
+        'schedule': 86400.0,
+        'args': ['weather in Kyiv']
+    },
+}
