95ee95115ee5fb759aa8155670c07fc4233c2053
alizarinston <alizarinstone@gmail.com>
2019-09-29 20:20:07 +0300

    added auth to frontend

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -1,16 +1,24 @@
 import React, {Component} from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { connect } from 'react-redux';
 import BaseRouter from './routes';
 import 'antd/dist/antd.css';
+import * as actions from './store/actions/auth'
 
 import CustomLayout from './containers/Layout';
 
 class App extends Component {
+
+  componentDidMount() {
+    this.props.onTryAutoSignup();
+    
+  }
+
   render() {
     return (
-    <div className="App">
+    <div>
       <Router>
-        <CustomLayout>
+        <CustomLayout {...this.props}>
           <BaseRouter/>
         </CustomLayout>
       </Router>
@@ -19,4 +27,16 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+      isAuthenticated: state.token !== null
+  }
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState())
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- frontend/gui/src/routes.js
+++ frontend/gui/src/routes.js
@@ -5,6 +5,8 @@ import MarketList from './containers/MarketListView';
 import MarketDetail from "./containers/MarketDetailView";
 import ProposalList from "./containers/ProposalListView";
 import ProposalDetail from "./containers/ProposalDetailView";
+import Login from './containers/Login';
+import Signup from './containers/Signup';
 
 const BaseRouter = () => (
     <div>
@@ -12,6 +14,8 @@ const BaseRouter = () => (
         <Route exact path='/markets/:marketID' component={MarketDetail}/>
         <Route exact path='/proposals/' component={ProposalList}/>
         <Route exact path='/proposals/:proposalID' component={ProposalDetail}/>
+        <Route exact path='/login/' component={Login}/>
+        <Route exact path='/signup/' component={Signup}/>
     </div>
 );
 
