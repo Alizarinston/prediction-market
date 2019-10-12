1e17b6c36f6afc80f4a5248fb6ab65e731cf9052
alizarinston <alizarinstone@gmail.com>
2019-10-12 03:38:31 +0300

    уотакуот

--- frontend/gui/src/components/Proposal.js
+++ frontend/gui/src/components/Proposal.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Rail, Segment, Header, Progress, Statistic, Icon, Label, Container, Card } from 'semantic-ui-react'
+import { Rail, Segment, Header, Progress, Statistic, Icon, Label, Container, Card, Popup, Button, Grid } from 'semantic-ui-react'
 import Countdown from "./Countdown";
 import { Link } from 'react-router-dom';
 
@@ -15,28 +15,48 @@ const Proposals = (props) => {
     }
 
     function category(itemCategory) {
-        if (itemCategory === 'Sports') {
+        if (itemCategory === 'SPO') {
             return ({
                 icon: "futbol",
-                color: 'green'
+                color: 'green',
+                title: 'Sports'
             })
-        } else if (itemCategory === 'Politics') {
+        } else if (itemCategory === 'POL') {
             return ({
                 icon: "handshake",
-                color: 'blue'
+                color: 'blue',
+                title: 'Politics'
             })
-        } else if (itemCategory === 'Finances') {
+        } else if (itemCategory === 'FIN') {
             return ({
                 icon: "dollar",
-                color: 'red'
+                color: 'red',
+                title: 'Finances'
             })
-        } else if (itemCategory === 'Other') {
+        } else if (itemCategory === 'OTH') {
             return ({
                 icon: "question",
-                color: 'grey'
+                color: 'grey',
+                title: 'Other'
             })
         }
     }
+
+    const colors = [
+      'red',
+      'orange',
+      'yellow',
+      'olive',
+      'green',
+      'teal',
+      'blue',
+      'violet',
+      'purple',
+      'pink',
+      'brown',
+      'grey',
+      'black',
+    ];
 
 
     return (
@@ -45,80 +65,114 @@ const Proposals = (props) => {
 
             {props.data.map(item => (
                 <div>
-                    <br/><br/>
-                    <Card link fluid>
-                        <Segment.Group stacked key={item.id} piled raised>
+                    <br/><br/><br/>
+                    <Popup position={"bottom center"} hoverable flowing basic pinned
+                        trigger={
+                            <Card link fluid>
+                                <Segment.Group stacked key={item.id} piled raised>
 
-                            <Link to={`/proposals/${item.id}`}>
-                                <Segment padded stacked size={"huge"}>
-
-                                    <Label as='a' color={category(item.categories)['color']} tag attached={"top right"} size={"small"}>
-                                        <Icon name={category(item.categories)['icon']}/>
-                                        {item.categories}
-                                    </Label>
-
-                                    {
-                                        <a>
-                                            <Header as='h2' >
-                                                {item.name}
-                                            </Header>
-                                        </a>
-                                    }
-
-                                </Segment>
-                            </Link>
-
-                            <Link to={`/proposals/${item.id}`}>
-                                <Segment>
-                                    <Container text textAlign={"center"}>
-                                        <Header as={"h3"}>
-                                            {item.description}
-                                        </Header>
-                                    </Container>
-                                </Segment>
-                            </Link>
-
-                            <Link to={`/proposals/${item.id}`}>
-                                <Segment basic raised>
-                                    <Label attached='bottom' ribbon color={checkColor(new Date(Date.now()).getDate(), new Date(item.end_date).getDate())}>
-                                        <Statistic.Group>
-
-                                            <Statistic>
-                                                <Statistic.Value>
-                                                    <Label attached={"bottom"}>{item.end_date}</Label>
-                                                </Statistic.Value>
-                                            </Statistic>
-
-                                            <Statistic>
-                                                <Statistic.Value>
-                                                    <Label attached={"bottom left"}>Supply: {item.supply} <Icon name={"bolt"}/></Label>
-                                                </Statistic.Value>
-                                            </Statistic>
-
-                                            <Statistic>
-                                                <Statistic.Value>
-                                                    <Countdown date={item.end_date}/>
-                                                </Statistic.Value>
-                                            </Statistic>
-
-                                        </Statistic.Group>
-                                    </Label>
-
-                                    <Progress percent={new Date(Date.now()).getDate() * 100 / new Date(item.end_date).getDate()}
-                                              attached={"bottom"}
-                                              color={checkColor(new Date(Date.now()).getDate(), new Date(item.end_date).getDate())}/>
-
-                                </Segment>
-                            </Link>
+                                    <Link to={`/proposals/${item.id}`}>
+                                        <Segment padded stacked size={"huge"}>
 
 
-                            <Rail dividing position='right'>
-                                <Segment>{item.outcomes}</Segment>
-                            </Rail>
+                                            <Label as='a' color={category(item.category)['color']} tag
+                                                   attached={"top right"} size={"small"}>
+                                                <Icon name={category(item.category)['icon']}/>
+                                                {category(item.category)['title']}
+                                            </Label>
 
 
-                        </Segment.Group>
-                    </Card>
+                                            {
+                                                <a>
+                                                    <Header as='h2'>
+                                                        {item.name}
+                                                    </Header>
+                                                </a>
+                                            }
+
+                                        </Segment>
+                                    </Link>
+
+                                    <Link to={`/proposals/${item.id}`}>
+                                        <Segment>
+                                            <Container text textAlign={"center"}>
+                                                <Header as={"h3"}>
+                                                    {item.description}
+                                                </Header>
+                                            </Container>
+                                        </Segment>
+                                    </Link>
+
+                                    <Link to={`/proposals/${item.id}`}>
+                                        <Segment basic raised>
+                                            <Label attached='bottom' ribbon
+                                                   color={checkColor(new Date(Date.now()).getDate(), new Date(item.end_date).getDate())}>
+                                                <Statistic.Group>
+
+                                                    <Statistic>
+                                                        <Statistic.Value>
+                                                            <Label attached={"bottom"}>{item.end_date}</Label>
+                                                        </Statistic.Value>
+                                                    </Statistic>
+
+                                                    <Statistic>
+                                                        <Statistic.Value>
+                                                            <Label attached={"bottom left"}>Supply: {item.supply} <Icon
+                                                                name={"bolt"}/></Label>
+                                                        </Statistic.Value>
+                                                    </Statistic>
+
+                                                    <Statistic>
+                                                        <Statistic.Value>
+                                                            <Countdown date={item.end_date}/>
+                                                        </Statistic.Value>
+                                                    </Statistic>
+
+                                                </Statistic.Group>
+                                            </Label>
+
+                                            <Progress
+                                                percent={new Date(Date.now()).getDate() * 100 / new Date(item.end_date).getDate()}
+                                                attached={"bottom"}
+                                                color={checkColor(new Date(Date.now()).getDate(), new Date(item.end_date).getDate())}/>
+
+                                        </Segment>
+                                    </Link>
+
+
+                                    <Rail dividing position='right'>
+                                        <Segment>
+                                            {/*<Popup trigger={<Segment/>} flowing hoverable>
+
+                                                {item.outcomes.map(o => (
+                                                    <Segment key={o.id}>{o.description}</Segment>
+                                                ))}
+
+                                            </Popup>*/}
+
+                                            {/*{item.outcomes.map( o => (
+                                   <Segment key={o.id}>{o.description}</Segment>
+                                ))}*/}
+
+                                        </Segment>
+                                    </Rail>
+
+
+                                </Segment.Group>
+                            </Card>
+                        }>
+
+                        {/*<Grid padded columns={(6 < item.outcomes.length) ? (5 % item.outcomes.length) : item.outcomes.length}>*/}
+                            {item.outcomes.map((o, num) => (
+                                //{/*<Grid.Column>*/}
+                                <Button.Group basic color={colors[num%13]} vertical attached={"bottom"} compact>
+                                    <Button primary attached key={o.id}>{o.description}</Button>
+                                </Button.Group>
+                                // <Column>
+                            ))}
+                        {/*</Grid>*/}
+
+                    </Popup>
                 </div>
 
             ))}
